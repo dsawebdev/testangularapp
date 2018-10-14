c97128fd0bb7197204b4fd5aa066354149d6c840
Devon Anderson <dvonanderson@gmail.com>
2018-10-14 14:31:51 -0400

    minor changes

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -12,6 +12,7 @@ export class UsersComponent implements OnInit {
   showExtended: boolean = true;
   loaded: boolean = true;
   enableAdd: Boolean = true;
+  showUserForm: boolean = !true;
 
   constructor() { }
 
